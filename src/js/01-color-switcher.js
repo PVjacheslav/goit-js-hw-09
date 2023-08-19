d9a812091481942eba0cb4342d82d593c3d4b14f
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-19 08:00:35 +0300

    solve tasck1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  //   body.style.backgroundColor = getRandomHexColor();
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    btnStart.disabled = true;
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  btnStart.disabled = false;
+  //   btnStop.disabled = true;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
