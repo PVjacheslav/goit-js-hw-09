c96af3346eccecaf619c1f6462bf671ee0ebda1a
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-20 21:11:06 +0300

    begin 3task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,11 +7,36 @@ elements = {
   amount: document.querySelector('input[name="amount"]'),
 };
 
+elements.form.addEventListener('submit', submit);
+
+function submit(evt) {
+  evt.preventDefault();
+  const delay = Number(elements.delay.value);
+  const step = Number(elements.step.value);
+  const amount = Number(elements.amount.value);
+  let position;
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay);
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+  createPromise(position, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
